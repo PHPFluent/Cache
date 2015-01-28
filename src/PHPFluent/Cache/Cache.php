50addbdb5ba15dbcf1ca67ebedbbe84013692e6e
Henrique Moody <henriquemoody@gmail.com>
2015-01-28 12:12:00 -0200

    Fix some coding standards

--- src/PHPFluent/Cache/Cache.php
+++ src/PHPFluent/Cache/Cache.php
@@ -4,27 +4,27 @@ namespace PHPFluent\Cache;
 class Cache implements \ArrayAccess
 {
     private $cache;
- 
+
     public function __construct(\Doctrine\Common\Cache\Cache $cache)
     {
         $this->cache = $cache;
     }
- 
+
     public function offsetExists($id)
     {
         return $this->cache->contains($id);
     }
- 
+
     public function offsetGet($id)
     {
         return $this->cache->fetch($id);
     }
- 
+
     public function offsetSet($id, $data)
     {
         return $this->cache->save($id, $data);
     }
- 
+
     public function offsetUnset($id)
     {
         return $this->cache->delete($id);
